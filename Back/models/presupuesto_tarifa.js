3ecec9443e97fbce210fd6e8622830da205b1d6d
carlosrluc <202121042@urp.edu.pe>
2024-10-06 23:43:29 -0500

    s

--- Back/models/presupuesto_tarifa.js
+++ Back/models/presupuesto_tarifa.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 
 var PresupuestoTarifaSchema = Schema({
   ID_Presupuesto: { type: Schema.ObjectId, ref: 'presupuesto', required: true },
-  ID_tarifa: { type: Schema.ObjectId, ref: 'tarifa', required: true },
+  ID_tarifa: { type: Schema.ObjectId, ref: 'instalacion', required: true },
   Cantidad: { type: Number, required: true },
   Costo_Total: { type: Number, required: true }
 });
