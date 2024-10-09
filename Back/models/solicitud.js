b3f5d67cc9daa7f25b48d930057fb6ce707a804e
carlosrluc <202121042@urp.edu.pe>
2024-10-09 00:08:56 -0500

    Update solicitud.js

--- Back/models/solicitud.js
+++ Back/models/solicitud.js
@@ -4,16 +4,18 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var SolicitudSchema = Schema({
- id: {type: Number, required: true},
- vendedor: {type: Schema.ObjectId, ref: 'usuario', required: false},
- cliente: {type: Schema.ObjectId, ref:'cliente', required: false},
- caracteristicas_obra: {type: String, required: true},
- descripcion_servicio: {type: String, required: true},
- observaciones: {type: String, default: 'No hay observaciones', required: false},
- estado_1: {type: String, default: 'Enviado', required: false},
- estado_2: {type: String, default: 'Pendiente', required: false},
- presupuesto: {type: Number, ref: 'presupuesto', required: false}, //modificado nuevamente a number, ya que presupuesto no puede ser un ID
- createdAt: {type: Date, default: Date.now, required: true}
-});
-
+    id: {type: Number, required: true},
+    vendedor: {type: Schema.ObjectId, ref: 'usuario', required: false},
+    cliente: {type: Schema.ObjectId, ref:'cliente', required: false},
+    caracteristicas_obra: {type: String, required: true},
+    descripcion_servicio: {type: String, required: true},
+    observaciones: {type: String, default: 'No hay observaciones', required: false},
+    distrito: {type:String, required: false},
+    direccion: {type:String, required: false},
+    estado_1: {type: String, default: 'Enviado', required: false},
+    estado_2: {type: String, default: 'Pendiente', required: false},
+    presupuesto: {type: Number, ref: 'presupuesto', required: false}, //modificado nuevamente a number, ya que presupuesto no puede ser un ID
+    createdAt: {type: Date, default: Date.now, required: true}
+   });
+   
 module.exports = mongoose.model('solicitud', SolicitudSchema ); 
