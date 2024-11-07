f5a41b5e99570c2790689e323e658ac208fa881e
Dav1d963 <giussephe.an@gmail.com>
2024-11-07 03:12:17 -0500

    Volver responsive las paginas de gestionar clientes, preupuestos, tarifa, solicitud, y gestionar clientes del vendedor

--- front/src/pages/Administrador/GestionSolicitud.jsx
+++ front/src/pages/Administrador/GestionSolicitud.jsx
@@ -1,18 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { listarSolicitudesRequest, actualizarEstadoSolicitudRequest } from '../../api/auth';
+import { listarSolicitudesRequest } from '../../api/auth';
 import MenuSideBar from '../../components/MenuSideBar';
 import NavBar from '../../components/NavBar';
-import { Link } from 'react-router-dom';
-import { confirmAlert } from 'react-confirm-alert';
-import 'react-confirm-alert/src/react-confirm-alert.css';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const GestionSolicitud = () => {
     const [solicitudes, setSolicitudes] = useState([]);
     const [drawerOpen, setDrawerOpen] = useState(false);
     const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 5; // Número máximo de solicitudes por página
+    const itemsPerPage = 5;
     const navigate = useNavigate();
+
     useEffect(() => {
         const fetchSolicitudes = async () => {
             try {
@@ -22,120 +20,104 @@ const GestionSolicitud = () => {
                 console.error('Error al listar las solicitudes:', error);
             }
         };
-
         fetchSolicitudes();
     }, []);
-
-    /* const handleEstadoChange = async (id, nuevoEstado) => {
-        try {
-            await actualizarEstadoSolicitudRequest(id, nuevoEstado);
-            setSolicitudes(solicitudes.map(solicitud =>
-                solicitud._id === id ? { ...solicitud, estado_1: nuevoEstado } : solicitud
-            ));
-        } catch (error) {
-            console.error('Error al actualizar el estado:', error);
-        }
-    };*/
 
     const handleDrawerToggle = () => {
         setDrawerOpen(!drawerOpen);
     };
 
-          // Calcular las solicitudes que se mostrarán en la página actual
-  const indexOfLastSolicitud = currentPage * itemsPerPage;
-  const indexOfFirstSolicitud = indexOfLastSolicitud - itemsPerPage;
-  const currentSolicitudes = solicitudes.slice(indexOfFirstSolicitud, indexOfLastSolicitud);
-  const totalPages = Math.ceil(solicitudes.length / itemsPerPage);
+    const indexOfLastSolicitud = currentPage * itemsPerPage;
+    const indexOfFirstSolicitud = indexOfLastSolicitud - itemsPerPage;
+    const currentSolicitudes = solicitudes.slice(indexOfFirstSolicitud, indexOfLastSolicitud);
+    const totalPages = Math.ceil(solicitudes.length / itemsPerPage);
 
-  const handlePageChange = (newPage) => {
-    setCurrentPage(newPage);
-  };
-
-    const renderSolicitudes = () => {
-      if (solicitudes == null) {
-        return (
-          <div className="flex justify-center items-center mt-10">
-          <div className="bg-gray-100 border border-gray-300 rounded-lg shadow-md p-6 text-center">
-            <h2 className="text-xl font-semibold text-gray-700 mb-2">No existen solicitudes actualmente</h2>
-            <p className="text-gray-500">No se han registrado solicitudes en el sistema por el momento.</p>
-          </div>
-        </div>
-        );
-      } else {
-        //si el estado dos esta pendiente no se debe visualizar
-        //poner botton en el check, x, y ver
-        //quitar los botones cuando se presiona uno de ellos y de ahi aparesca el estado en el que esta 
-        //QUE LOS BOTONES TENGAN EFECTO EN ESTADO_1 Y 2
-        ///poner cliente y vendedora en ves de descripcion y caracteristica
-        ///que los botones check y x esten en InfoSolicitud.jsx
-        //mandar popup "estas seguro" cuando se busca aprobar o X 
-        //usar comfirm alert
-        return (
-          <table className="shadow-md w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-              <tr>
-                <th className="px-6 py-3">ID</th>
-                <th className="px-6 py-3">Característica</th>
-                <th className="px-6 py-3">Descripción</th>
-                <th className="px-6 py-3">Estado</th>
-                <th className="px-6 py-3">Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              {currentSolicitudes.map((solicitud) => (
-                <tr key={solicitud._id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                  <td className="px-6 py-4">{solicitud.id}</td>
-                  <td className="px-6 py-4">{solicitud.caracteristicas_obra}</td>
-                  <td className="px-6 py-4">{solicitud.descripcion_servicio}</td>
-                  <td className="px-6 py-4">{solicitud.estado_2}</td>
-                  <td className="px-6 py-4">
-                    <button
-                      className="bg-blue-500 text-black px-4 py-2 rounded"
-                      onClick={() => navigate(`/info_solicitud/${solicitud._id}`)}
-                    >
-                      Ver
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        );
-      }
+    const handlePageChange = (newPage) => {
+        if (newPage > 0 && newPage <= totalPages) {
+            setCurrentPage(newPage);
+        }
     };
 
-  
     return (
-      <div className="flex">
-        <MenuSideBar open={drawerOpen} />
-        <div className="flex-1">
-          <NavBar onDrawerToggle={handleDrawerToggle} drawerOpen={drawerOpen} />
-          <div className="p-6">
-        
-            <h1 className="text-3xl font-bold mb-6">Gestión de Solicitud</h1>
-            {/* Renderizar las solicitudes o mensaje */}
-            {renderSolicitudes()}
-          
-           {/* Paginación */}
-           <div className="flex justify-center mt-4">
-              <button 
-                onClick={() => handlePageChange(currentPage - 1)} 
-                disabled={currentPage === 1}
-                className="px-4 py-2 border rounded-l-lg bg-gray-200 hover:bg-gray-300">
-                Anterior
-              </button>
-              <span className="px-4 py-2">{`Página ${currentPage} de ${totalPages}`}</span>
-              <button 
-                onClick={() => handlePageChange(currentPage + 1)} 
-                disabled={currentPage === totalPages}
-                className="px-4 py-2 border rounded-r-lg bg-gray-200 hover:bg-gray-300">
-                Siguiente
-              </button>
+        <div className="flex">
+            <MenuSideBar open={drawerOpen} />
+            <div className="flex-1">
+                <NavBar onDrawerToggle={handleDrawerToggle} drawerOpen={drawerOpen} />
+                <div className="p-6">
+                    <h1 className="text-3xl font-bold mb-6">Gestión de Solicitud</h1>
+                    
+                    {/* Tabla para pantallas grandes */}
+                    <table className="hidden md:block shadow-md w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th className="px-6 py-3">ID</th>
+                                <th className="px-6 py-3">Característica</th>
+                                <th className="px-6 py-3">Descripción</th>
+                                <th className="px-6 py-3">Estado</th>
+                                <th className="px-6 py-3">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {currentSolicitudes.map((solicitud) => (
+                                <tr key={solicitud._id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                    <td className="px-6 py-4">{solicitud.id}</td>
+                                    <td className="px-6 py-4">{solicitud.caracteristicas_obra}</td>
+                                    <td className="px-6 py-4">{solicitud.descripcion_servicio}</td>
+                                    <td className="px-6 py-4">{solicitud.estado_2}</td>
+                                    <td className="px-6 py-4">
+                                        <button
+                                            className="bg-blue-500 text-black px-4 py-2 rounded"
+                                            onClick={() => navigate(`/info_solicitud/${solicitud._id}`)}
+                                        >
+                                            Ver
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+
+                    {/* Tabla para pantallas pequeñas */}
+                    <div className="space-y-4 md:hidden">
+                        {currentSolicitudes.map((solicitud) => (
+                            <div key={solicitud._id} className="bg-white p-4 rounded-lg shadow">
+                                <div className="text-sm font-bold">ID: {solicitud.id}</div>
+                                <div className="text-sm">Característica: {solicitud.caracteristicas_obra}</div>
+                                <div className="text-sm">Estado: {solicitud.estado_2}</div>
+                                <div className="pt-2">
+                                    <button
+                                        className="bg-blue-500 text-black px-4 py-2 rounded"
+                                        onClick={() => navigate(`/info_solicitud/${solicitud._id}`)}
+                                    >
+                                        Ver
+                                    </button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                    {/* Paginación */}
+                    <div className="flex justify-center mt-4">
+                        <button
+                            onClick={() => handlePageChange(currentPage - 1)}
+                            disabled={currentPage === 1}
+                            className="px-4 py-2 border rounded-l-lg bg-gray-200 hover:bg-gray-300"
+                        >
+                            Anterior
+                        </button>
+                        <span className="px-4 py-2">{`Página ${currentPage} de ${totalPages}`}</span>
+                        <button
+                            onClick={() => handlePageChange(currentPage + 1)}
+                            disabled={currentPage === totalPages}
+                            className="px-4 py-2 border rounded-r-lg bg-gray-200 hover:bg-gray-300"
+                        >
+                            Siguiente
+                        </button>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     );
-  };
-  
-  export default GestionSolicitud;
+};
+
+export default GestionSolicitud;
--- front/src/pages/Administrador/GestionarPresupuestos.jsx
+++ front/src/pages/Administrador/GestionarPresupuestos.jsx
@@ -1,46 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import {listarPresupuestosRequest, obtenerSolicitudPorIdRequest, obtener_cliente_por_idRequest, obtener_usuario_por_idRequest } from '../../api/auth'; // Asegúrate de que este request esté en auth.js
+import { listarPresupuestosRequest } from '../../api/auth';
 import MenuSideBar from '../../components/MenuSideBar';
 import NavBar from '../../components/NavBar';
 import { Link } from 'react-router-dom';
 
 const GestionarPresupuestosA = () => {
   const [presupuestos, setPresupuestos] = useState([]);
-  const [presupuestos2, setPresupuestos2] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [drawerOpen, setDrawerOpen] = useState(false);
-  const [cliente, setCliente] = useState('');
-  const [nombreVendedor, setNombreVendedor] = useState('');
-  const [Solicitud, setSolicitudes] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const itemsPerPage = 5; // Número máximo de presupuestos por página
+  const itemsPerPage = 5;
 
   useEffect(() => {
     const fetchPresupuestos = async () => {
       try {
         const token = localStorage.getItem('token');
         const headers = {
-          'Authorization': `Bearer ${token}`,
+          Authorization: `Bearer ${token}`,
           'Content-Type': 'application/json',
         };
         const response = await listarPresupuestosRequest(headers);
-       // const prep = response.data.data;
-        console.log(response.data.data);
-      /*  const solicitudes = await Promise.all(
-          prep.map(async (presupuesto) => {
-            const solicitudRes = await obtenerSolicitudPorIdRequest(presupuesto.ID_Solicitud_Presupuesto._id);
-            const solicitud = solicitudRes.data.data;
-            console.log(solicitudRes.data.data)
-            const clienteRes = await obtener_cliente_por_idRequest(solicitudRes.data.data.cliente._id);
-            const cliente = clienteRes.data.data;
-            const vendedorRes = await obtener_cliente_por_idRequest(solicitudRes.data.data.cliente._id);
-            const vendedor = clienteRes.data.data;
-            return { solicitud, cliente, vendedor };
-          })
-        );*/
         setPresupuestos(response.data.data);
-        //setSolicitudes(solicitudes);
-        setIsLoading(false);
       } catch (error) {
         console.error('Error al listar los presupuestos:', error.response ? error.response.data : error.message);
       }
@@ -48,33 +27,7 @@ const GestionarPresupuestosA = () => {
 
     fetchPresupuestos();
   }, []);
- /* useEffect(() => {
-    const fetchSolicitudes = async () => {
-      try {
-        const solicitudes = await Promise.all(
-          presupuestos.map(async (presupuesto) => {
-            const solicitudRes = await obtenerSolicitudPorIdRequest(presupuesto.ID_Solicitud_Presupuesto);
-            const solicitud = solicitudRes.data.data;
-            const clienteRes = await obtener_cliente_por_idRequest(solicitud.cliente._id);
-            const cliente = clienteRes.data.data;
-            return { solicitud, cliente };
-          })
-        );
-        setSolicitudes(solicitudes);
-        console.log(presupuestos)
-        console.log(Solicitud)
-        setIsLoading(false);
-      } catch (error) {
-        console.error('Error al obtener la solicitud:', error);
-      }
-    };
-  
-    if (presupuestos2.length > 0) {
-      fetchSolicitudes();
-    }
-  }, [presupuestos2]); */
-  
-  // Calcular los presupuestos que se mostrarán en la página actual
+
   const indexOfLastPresupuesto = currentPage * itemsPerPage;
   const indexOfFirstPresupuesto = indexOfLastPresupuesto - itemsPerPage;
   const currentPresupuestos = presupuestos.slice(indexOfFirstPresupuesto, indexOfLastPresupuesto);
@@ -93,68 +46,77 @@ const GestionarPresupuestosA = () => {
       <MenuSideBar open={drawerOpen} />
       <div className="flex-1">
         <NavBar onDrawerToggle={handleDrawerToggle} drawerOpen={drawerOpen} />
-        <div className="p-6">
-          <div className="relative overflow-x-auto sm:rounded-lg">
-            <div className='select-none flex items-center justify-between mb-4'>
-              <h1 className="text-3xl font-bold mb-2">Gestor de Presupuestos</h1>
-              <div className="flex justify-end mb-4">
+        <div className="p-6 mt-16 h-full">
+          <div className="relative overflow-hidden h-full">
+            <div className="overflow-y-auto h-[calc(100vh-6rem)]">
+              <div className="flex items-center justify-between mb-4">
+                <h1 className="text-3xl font-bold mb-2">Gestor de Presupuestos</h1>
                 <Link to="/registro_presupuestos">
                   <button className="text-white bg-green-800 hover:bg-green-900 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2">
                     Crear Presupuesto
                   </button>
                 </Link>
-              </div>  
-            </div>
-            <table className="shadow-md w-full text-sm text-left text-gray-500 dark:text-gray-400">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                  
-                  <th scope="col" className="px-6 py-3">ID Solicitud</th>
-                  <th scope="col" className="px-6 py-3">Nombre del cliente</th>
-                  <th scope="col" className="px-6 py-3">Nombre del vendedor</th>
-                  <th scope="col" className="px-6 py-3">Costo Materiales</th>
-                  <th scope="col" className="px-6 py-3">Acciones</th>
-                </tr>
-              </thead>
-              <tbody>
-                {currentPresupuestos.map((presupuestos, index) =>{ 
-                
-                // Obtiene el cliente si la solicitud es válida
-               // const clientes = solicitud && solicitud.cliente ? cliente[solicitud.cliente._id] : null;
+              </div>
 
-                  return(
-                  <tr key={presupuestos._id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <td className="px-6 py-4 text-gray-900 dark:text-white">{presupuestos.ID_Solicitud_Presupuesto?.id}</td>
-                    <td className="px-6 py-4 text-gray-900 dark:text-white">{presupuestos.ID_Solicitud_Presupuesto?.cliente.nombre} {presupuestos.ID_Solicitud_Presupuesto?.cliente.apellidos}</td>
-                    <td className="px-6 py-4 text-gray-900 dark:text-white">{presupuestos.ID_Solicitud_Presupuesto.vendedor.nombre} {presupuestos.ID_Solicitud_Presupuesto.vendedor.apellidos}</td>
-                    <td className="px-6 py-4 text-gray-900 dark:text-white">{presupuestos?.Costo_Materiales}</td>
-                    <td className="px-6 py-4 text-gray-900 dark:text-white">  
-                      <Link to={`/visualizar_presupuesto/${presupuestos._id}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline mr-4">
-                        Ver
-                      </Link>
-                      <Link to={`/editar_presupuesto/${presupuestos._id}`} className="font-medium text-green-600 dark:text-green-500 hover:underline mr-4">
-                        Editar
-                      </Link>
-                    </td>
-                  </tr> 
-                )})}
-              </tbody>
-            </table>
-            {/* Paginación */}
-            <div className="flex justify-center mt-4">
-              <button 
-                onClick={() => handlePageChange(currentPage - 1)} 
-                disabled={currentPage === 1}
-                className="px-4 py-2 border rounded-l-lg bg-gray-200 hover:bg-gray-300">
-                Anterior
-              </button>
-              <span className="px-4 py-2">{`Página ${currentPage} de ${totalPages}`}</span>
-              <button 
-                onClick={() => handlePageChange(currentPage + 1)} 
-                disabled={currentPage === totalPages}
-                className="px-4 py-2 border rounded-r-lg bg-gray-200 hover:bg-gray-300">
-                Siguiente
-              </button>
+              {/* Tabla para PC */}
+              <table className="hidden md:block shadow-md w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                    <th className="px-6 py-3">ID Solicitud</th>
+                    <th className="px-6 py-3">Nombre del cliente</th>
+                    <th className="px-6 py-3">Nombre del vendedor</th>
+                    <th className="px-6 py-3">Costo Materiales</th>
+                    <th className="px-6 py-3">Acciones</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {currentPresupuestos.map((presupuesto) => (
+                    <tr key={presupuesto._id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                      <td className="px-6 py-4 text-gray-900 dark:text-white">{presupuesto.ID_Solicitud_Presupuesto?.id}</td>
+                      <td className="px-6 py-4 text-gray-900 dark:text-white">{presupuesto.ID_Solicitud_Presupuesto?.cliente.nombre} {presupuesto.ID_Solicitud_Presupuesto?.cliente.apellidos}</td>
+                      <td className="px-6 py-4 text-gray-900 dark:text-white">{presupuesto.ID_Solicitud_Presupuesto.vendedor.nombre} {presupuesto.ID_Solicitud_Presupuesto.vendedor.apellidos}</td>
+                      <td className="px-6 py-4 text-gray-900 dark:text-white">{presupuesto?.Costo_Materiales}</td>
+                      <td className="px-6 py-4 text-gray-900 dark:text-white">
+                        <Link to={`/visualizar_presupuesto/${presupuesto._id}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline mr-4">Ver</Link>
+                        <Link to={`/editar_presupuesto/${presupuesto._id}`} className="font-medium text-green-600 dark:text-green-500 hover:underline mr-4">Editar</Link>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+
+              {/* Vista para móviles */}
+              <div className="space-y-6 md:hidden">
+                {currentPresupuestos.map((presupuesto) => (
+                  <div key={presupuesto._id} className="bg-white p-4 space-y-3 rounded-lg shadow">
+                    <div className="text-sm font-bold">{`ID Solicitud: ${presupuesto.ID_Solicitud_Presupuesto?.id}`}</div>
+                    <div className="text-sm text-gray-700">{`Vendedor: ${presupuesto.ID_Solicitud_Presupuesto.vendedor.nombre} ${presupuesto.ID_Solicitud_Presupuesto.vendedor.apellidos}`}</div>
+                    <div className="text-sm text-gray-700">{`Cliente: ${presupuesto.ID_Solicitud_Presupuesto?.cliente.nombre} ${presupuesto.ID_Solicitud_Presupuesto?.cliente.apellidos}`}</div>
+                    <div className="text-sm text-gray-700">{`Costo Materiales: ${presupuesto?.Costo_Materiales}`}</div>
+                    <div className="flex pt-2 items-center justify-center">
+                      <Link to={`/visualizar_presupuesto/${presupuesto._id}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline mr-4">Ver</Link>
+                      <Link to={`/editar_presupuesto/${presupuesto._id}`} className="font-medium text-green-600 dark:text-green-500 hover:underline">Editar</Link>
+                    </div>
+                  </div>
+                ))}
+              </div>
+
+              {/* Paginación */}
+              <div className="flex justify-center mt-4">
+                <button 
+                  onClick={() => handlePageChange(currentPage - 1)} 
+                  disabled={currentPage === 1}
+                  className="px-4 py-2 border rounded-l-lg bg-gray-200 hover:bg-gray-300">
+                  Anterior
+                </button>
+                <span className="px-4 py-2">{`Página ${currentPage} de ${totalPages}`}</span>
+                <button 
+                  onClick={() => handlePageChange(currentPage + 1)} 
+                  disabled={currentPage === totalPages}
+                  className="px-4 py-2 border rounded-r-lg bg-gray-200 hover:bg-gray-300">
+                  Siguiente
+                </button>
+              </div>
             </div>
           </div>
         </div>
