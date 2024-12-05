2700ac4141774a6afae66168721fec49de0e4d88
minaya-v <valminaya@gmail.com>
2024-12-05 06:04:36 -0500

    cambios diseño: admin

--- front/src/pages/Administrador/GestionarAlmacen.jsx
+++ front/src/pages/Administrador/GestionarAlmacen.jsx
@@ -124,7 +124,7 @@ const GestionarAlmacen = () => {
             </div>
 
             {/* Tabla de Almacenes */}
-            <table className="shadow-md w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <table className="shadow-md w-full overflow-hidden rounded-lg text-sm text-left text-gray-500 dark:text-gray-400">
               <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                   <th scope="col" className="px-6 py-3">Nombre</th>
